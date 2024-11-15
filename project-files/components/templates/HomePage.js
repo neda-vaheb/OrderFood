7aa07ba0b08230add4c137ae31235ecc33827b2f
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-11-15 04:23:11 -0800

    homePage was completed

--- project-files/components/templates/HomePage.js
+++ project-files/components/templates/HomePage.js
@@ -2,6 +2,9 @@ import Definition from "../modules/Definition";
 import Guide from "../modules/Guide";
 import Instruction from "../modules/Instruction";
 import Restrictions from "../modules/Restrictions";
+import Banner from "../modules/Banner";
+import Atributes from "../modules/Atributes";
+import Companies from "../modules/Companies";
 import styles from "./HomePage.module.css";
 
 function HomePage() {
--- project-files/pages/index.js
+++ project-files/pages/index.js
@@ -1,4 +1,4 @@
-import HomePage from"../components/template/HomePage"
+import HomePage from"../components/templates/HomePage"
 
 export default function Home() {
   return (
